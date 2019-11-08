8e62e527fcfc22a37500bab33d93a96053a228bb
arunkumarn <arunaiekhil@gmail.com>
2019-11-08 19:08:44 +0530

    Design changes.
    Changes input to button, applied native look.

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-var inputs = document.getElementById("inputsBox");
-var result = document.getElementById("resultBox");
+var input = document.getElementById('inputBox');
+var result = document.getElementById('resultBox');
+var trigger = false;
 var cal = {
   insertValue: function(data) {
-    inputs.value += data;
+    if (trigger) { input.value = ""; trigger = false; }
+    input.value += data;
+    this.evaluate();
   },
-  clearResultBox: function() {
-    inputs.value = "";
+  insertSymbol: function(symbol) {
+    input.value += symbol;
+    trigger = false;
+    this.evaluate();
+  },
+  insertBracket: function() {
+    
+  },
+  clear: function() {
+    input.value = "";
     result.value = "";
   },
-  computeResult: function() {
-    
-  },
-  percentage: function(symbol) {
-    inputs.value += symbol;
-    result.value = parseInt(inputs.value)/100;
-  },
   signed: function() {
-  
+    
   },
   decimal: function() {
-    if (inputs.value.includes(".")) {
-      inputs.value = inputs.value;
+    
+  },
+  compute: function() {
+    var data = input.value;
+    if(data == "") {
+      input.value = "";
     } else {
-      inputs.value += '.';
+      input.value = eval(data);
+      result.value = "";
+      trigger = true;
     }
   },
-  insertBrackets: function() {
-    
+  evaluate: function() {
+    try {
+      result.value = eval(input.value);
+    } catch(e) {
+      result.value = ""
+    }
   },
-  operation: function(symbol) {
-    
+  percentage: function(symbol) {
+    input.value += symbol;
+    result.value = parseInt(input.value, 10)/100;
   }
-};
+}
