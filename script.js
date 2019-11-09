22138b276e5e49983255d3acf6cb107bffd26d09
arunkumarn <arunaiekhil@gmail.com>
2019-11-09 22:58:55 +0530

    Updated Math-operations
    Changed design and updated math operations.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var input = document.getElementById('inputBox');
-var result = document.getElementById('resultBox');
+var input = document.getElementById("inputBox");
+var result = document.getElementById("resultBox");
 var trigger = false;
+var operators = ['+', '-', '*', '/'];
+
 var cal = {
   insertValue: function(data) {
-    if (trigger) { input.value = ""; trigger = false; }
+    if (trigger) {
+      input.value = "";
+      trigger = false;
+    }
     input.value += data;
     this.evaluate();
   },
@@ -12,38 +17,33 @@ var cal = {
     trigger = false;
     this.evaluate();
   },
-  insertBracket: function() {
-    
-  },
   clear: function() {
     input.value = "";
     result.value = "";
   },
-  signed: function() {
-    
-  },
   decimal: function() {
-    
+    input.value += '.';
   },
   compute: function() {
-    var data = input.value;
-    if(data == "") {
-      input.value = "";
-    } else {
-      input.value = eval(data);
+    if (input.value != "") {
+      input.value = eval(input.value);
       result.value = "";
       trigger = true;
     }
   },
   evaluate: function() {
     try {
-      result.value = eval(input.value);
-    } catch(e) {
-      result.value = ""
+        result.value = eval(input.value);
+    } catch (e) {
+      result.value = "";
     }
   },
   percentage: function(symbol) {
     input.value += symbol;
-    result.value = parseInt(input.value, 10)/100;
+    result.value = parseFloat(input.value) / 100;
+  },
+  backspace: function() {
+    input.value = input.value.slice(0, -1);
+    result.value = "";
   }
-}
+};
