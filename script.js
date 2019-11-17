0b275fbbd9069760e346a02d61334bdc0878d34f
arunkumarn <arunaiekhil@gmail.com>
2019-11-17 16:12:54 +0530

    Changed calculation methods

--- script.js
+++ script.js
@@ -1,49 +1,67 @@
-var input = document.getElementById("inputBox");
-var result = document.getElementById("resultBox");
-var trigger = false;
-var operators = ['+', '-', '*', '/'];
+var inputBox = document.getElementById("inputBox");
+var resultBox = document.getElementById("resultBox");
+var inputs = [];
+var value = "";
 
 var cal = {
   insertValue: function(data) {
-    if (trigger) {
-      input.value = "";
-      trigger = false;
-    }
-    input.value += data;
-    this.evaluate();
+    value += data;
   },
-  insertSymbol: function(symbol) {
-    input.value += symbol;
-    trigger = false;
-    this.evaluate();
+  insertOperator: function(op) {
+    if(value != "") { inputs.push(parseInt(value)); }
+    value = "";
+    inputs.push(op);
   },
-  clear: function() {
-    input.value = "";
-    result.value = "";
+  insertDecimal: function() {
+    
   },
-  decimal: function() {
-    input.value += '.';
-  },
-  compute: function() {
-    if (input.value != "") {
-      input.value = eval(input.value);
-      result.value = "";
-      trigger = true;
-    }
-  },
-  evaluate: function() {
-    try {
-        result.value = eval(input.value);
-    } catch (e) {
-      result.value = "";
-    }
-  },
-  percentage: function(symbol) {
-    input.value += symbol;
-    result.value = parseFloat(input.value) / 100;
-  },
-  backspace: function() {
-    input.value = input.value.slice(0, -1);
-    result.value = "";
+  finalExe: function() {
+    if(value != "") { inputs.push(parseInt(value)); }
+    value ="";
+    resultBox.value = evaluate(inputs);;
   }
+};
+
+
+function evaluate(inputs) {
+  var operatorIndex, operation;
+  Object.keys(calOperation).forEach(function(pre) {
+    while(inputs.includes(pre)) {
+      operatorIndex = inputs.indexOf(pre);
+      if(pre == '%') {
+    		var arg = Number(inputs[operatorIndex - 1])
+    		var total = calOperation[pre](arg);
+    		inputs.splice((operatorIndex-1),2, total);
+    		continue;
+    	}
+      inputs = calSequence(pre, operatorIndex, inputs);
+    }
+  });
+  return inputs;
+}
+
+const calOperation = {
+  '%': (arg1) => arg1 / 100,
+  '*': (arg1, arg2) => arg1 * arg2,
+  '/': (arg1, arg2) => arg1 / arg2,
+  '+': (arg1, arg2) => arg1 + arg2,
+  '-': (arg1, arg2) => arg1 - arg2,
+};
+
+const inputIndex = (index, inputs) => {
+  const arg1 = Number(inputs[index - 1]);
+  const arg2 = Number(inputs[index + 1]);
+  return [arg1, arg2];
+};
+
+const reduced = (index, newInputs, inputs) => {
+  inputs.splice((index-1),3,newInputs);
+  return inputs;
+};
+
+const calSequence = (op, operatorIndex, inputs) => {
+  const argument = inputIndex(operatorIndex, inputs);
+  const total = calOperation[op](argument[0], argument[1]);
+  const final = reduced(operatorIndex, total, inputs);
+  return final;
 };
