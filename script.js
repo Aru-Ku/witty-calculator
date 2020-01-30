80dd4785b6872076ad3b10c573ffb9cb788f9164
mastrero <arunaiekhil@gmail.com>
2020-01-31 00:51:05 +0530

    Major changes
    Re-writen HTML tags, changed CSS styling for responsiveness & updated script and calculation methods.

--- script.js
+++ script.js
@@ -1,111 +1,58 @@
-const inputBox = document.getElementById("inputBox");
-const resultBox = document.getElementById("resultBox");
-var inputs = []; // stores the inputs from the keys
-var value = ""; // stores 
-var l1 = 0, l2 = 0;
+const input = document.querySelector('.input');
+const result = document.querySelector('.result');
+const btns = document.querySelectorAll('input');
+const errors = [ '++', '+-', '+x', '+÷', '+%',
+                 '-+', '--', '-x', '-÷', '-%',
+                 'x+', 'x-', 'xx', 'x÷', 'x%',
+                 '÷+', '÷-', '÷x', '÷÷', '÷%',
+                 '%+', '%-', '%x', '%÷', '%%' ] // possible error occurances
 
-var cal = {
-  insertValue: function(data) {
-    value += data;
-    this.updateInputBox(data);
-  },
-  insertOperator: function(op) {
-    if(value != "") {
-      inputs.push(parseFloat(value));
-      value = "";
-    }
-    l1 = inputs.length;
-    if((inputs[l1-1] == '+') || (inputs[l1-1] == '-') || (inputs[l1-1] == '*') ||
-       (inputs[l1-1] == '/') || (inputs[l1-1] == '%') || (inputs[l1-1] == '.')) {
-      inputs[l1-1] = op;
-      this.updateInputBox('?');
-    } else {
-      inputs.push(op); 
-    }
-    this.updateInputBox(op);
-  },
-  insertDecimal: function() {
-    if(!value.includes('.')) {
-      value += '.';
-    }
-    this.updateInputBox('.');
-  },
-  backspace: function() {
-    l1 = inputs.length;
-    l2 = value.length;
-    resultBox.value = "";
-    if((inputs[l1-1] == '+') || (inputs[l1-1] == '-') || (inputs[l1-1] == '*') ||
-       (inputs[l1-1] == '/') || (inputs[l1-1] == '%') || (inputs[l1-1] == '.')) {
-      value += inputs[l1-2];
-      inputs = inputs.splice(0, l1-2);
-    } else {
-      value = value.substring(0, l2-1);
-    }
-    this.updateInputBox('?');
-  },
-  finalExe: function() {
-    if(value != "") { inputs.push(parseFloat(value)); }
-    resultBox.value = evaluate(inputs);
-    value = "";
-    value += inputs[0];
-    inputs = [];
-  },
-  clear: function() {
-    inputBox.value = "";
-    resultBox.value = "";
-    inputs = [];
-    value = [];
-  },
-  updateInputBox: function(str) {
-    if(str == '?') {
-      console.log(inputBox.value);
-      inputBox.value = inputBox.value.substring(0, inputBox.value.length-1);
-    } else {
-      inputBox.value += str;
-    }
-  }
-};
-
-
-function evaluate(inputs) {
-  var operatorIndex, operation;
-  Object.keys(calOperation).forEach(function(pre) {
-    while(inputs.includes(pre)) {
-      operatorIndex = inputs.indexOf(pre);
-      if(pre == '%') {
-    		var arg = Number(inputs[operatorIndex - 1])
-    		var total = calOperation[pre](arg);
-    		inputs.splice((operatorIndex-1),2, total);
-    		continue;
-    	}
-      inputs = calSequence(pre, operatorIndex, inputs);
-    }
-  });
-  return inputs;
+// clears the result box
+const clear = () => {
+    result.value = 0;
+    input.value = 0;
 }
-
-const calOperation = {
-  '%': (arg1) => arg1 / 100,
-  '*': (arg1, arg2) => arg1 * arg2,
-  '/': (arg1, arg2) => arg1 / arg2,
-  '+': (arg1, arg2) => arg1 + arg2,
-  '-': (arg1, arg2) => arg1 - arg2,
-};
-
-const inputIndex = (index, inputs) => {
-  const arg1 = Number(inputs[index - 1]);
-  const arg2 = Number(inputs[index + 1]);
-  return [arg1, arg2];
-};
-
-const reduced = (index, newInputs, inputs) => {
-  inputs.splice((index-1),3,newInputs);
-  return inputs;
-};
-
-const calSequence = (op, operatorIndex, inputs) => {
-  const argument = inputIndex(operatorIndex, inputs);
-  const total = calOperation[op](argument[0], argument[1]);
-  const final = reduced(operatorIndex, total, inputs);
-  return final;
-};
+// calculate the result
+const calculate = () => {
+    try {
+        result.value = eval(input.value.replace(/÷/g, '/').replace(/x/g, '*'));
+    } catch(e) {
+        if(errors.some(er => input.value.includes(er)))
+            result.value = "⚠ CHECK INPUTS";
+    }
+}
+// backspace function
+const backspace = () => {
+    input.value = input.value.slice(0, input.value.length - 1);
+}
+// percentage calculation
+const percentage = () => {
+    try {
+        input.value = eval(input.value.replace(/÷/g, '/').replace(/x/g, '*'))/100;
+    } catch(e) {
+        result.value = "OOPS!! Error calculating percentage";
+    }
+}
+// Validate input at each entry
+const validateAnswer = () => {
+    try { calculate(); }
+    catch(e) { return; }
+}
+// pulling data
+const gather = (e) => {
+    value = e.path[0].value;
+    switch(value) {
+        case 'C': clear(); break;
+        case '=': calculate(); break;
+        case '%': percentage(); break;
+        case "backspace": backspace(); break;
+        default : {
+            if(input.value == 0 && !input.value.includes('0.')) {
+                input.value = value;
+                break;
+            } input.value += value;
+        }
+    }
+    validateAnswer();
+}
+btns.forEach(btn => btn.addEventListener('click', gather));
